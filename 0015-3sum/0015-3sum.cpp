6d6c17de51fe0c978141b26f9eb3cfdbf3ee8389
Suraj Vitthal Patil <145219543+SurajPatil2645@users.noreply.github.com>
2025-02-06 22:43:43 +0530

    Time: 43 ms (86.73%), Space: 29.4 MB (45.75%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        if (nums.size() < 3) return {};  // Handle small input edge case
+
         sort(nums.begin(), nums.end());
         vector<vector<int>> answer;
+        answer.reserve(nums.size() / 3);  // Preallocate memory
 
-        for (int i = 0; i < nums.size(); i++) {
-            // Skip duplicate values for the first element
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int i = 0; i < nums.size() - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicate `i`
 
-            int j = i + 1;
-            int k = nums.size() - 1;
-
+            int j = i + 1, k = nums.size() - 1;
             while (j < k) {
                 int localSum = nums[i] + nums[j] + nums[k];
-                if (localSum < 0) {
-                    j++;
-                } else if (localSum > 0) {
-                    k--;
-                } else {
-                    answer.push_back({nums[i], nums[j], nums[k]});
-                    // Skip duplicate values for the second element
-                    while (j < k && nums[j] == nums[j + 1]) j++;
-                    // Skip duplicate values for the third element
-                    while (j < k && nums[k] == nums[k - 1]) k--;
-                    j++;
-                    k--;
-                }
+
+                if (localSum < 0) { j++; continue; }
+                if (localSum > 0) { k--; continue; }
+
+                answer.push_back({nums[i], nums[j], nums[k]});
+
+                // Skip duplicate `j` and `k`
+                do { j++; } while (j < k && nums[j] == nums[j - 1]);
+                do { k--; } while (j < k && nums[k] == nums[k + 1]);
             }
         }
         return answer;
