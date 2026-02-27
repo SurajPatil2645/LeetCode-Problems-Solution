3d839ab672efdae282d47dc62ab7ee19bc78ff35
Suraj Vitthal Patil <145219543+SurajPatil2645@users.noreply.github.com>
2026-02-27 22:30:17 +0530

    Time: 0 ms (100%), Space: 63 MB (10.84%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int max_result = 0;
-        int i = 0, j = height.size() - 1;
-
-        while (i < j) {
-            int area = (j - i) * min(height[i], height[j]); // Correct formula
-
-            // Update max_result if new area is larger
-            max_result = max(max_result, area);
-
-            // Move the pointer with the smaller height
-            if (height[i] < height[j]) {
-                i++;
-            } else {
-                j--;
-            }
+        int maxwt=0;
+        int lp=0, rp=height.size()-1;
+        while(lp<rp){
+            int w=rp-lp;
+            int ht=min(height[lp],height[rp]);
+            int curwt=w*ht;
+            maxwt=max(maxwt,curwt);
+            height[lp]<height[rp] ? lp++:rp--;
         }
-
-        return max_result;
+        return maxwt;
     }
 };
