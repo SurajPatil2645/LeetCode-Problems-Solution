dc783b6b106e17cc6a245eb8cc6372eca33ecfaf
Suraj Vitthal Patil <145219543+SurajPatil2645@users.noreply.github.com>
2026-03-12 19:38:57 +0530

    Time: 0 ms (100%), Space: 8.5 MB (65.39%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
+    int reverse(int n){
+        int revnum=0;
+        while(n !=0){
+            int dig=n%10;
+            if(revnum > INT_MAX/10 || revnum < INT_MIN/10){
+                return 0;
+            }
+            revnum=revnum*10+dig;
+            n=n/10;
+        }
+        return revnum;
+    }
     bool isPalindrome(int x) {
         if (x < 0) return false;
         
-        string s = to_string(x);
-        int n = s.length();
+        // string s = to_string(x);
+        // int n = s.length();
         
-        for (int i = 0; i < n / 2; i++) {
-            if (s[i] != s[n - 1 - i]) {
-                return false;
-            }
-        }
-        return true;
+        // for (int i = 0; i < n / 2; i++) {
+        //     if (s[i] != s[n - 1 - i]) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        int revnum=reverse(x);
+        return x==revnum;
     }
 };
