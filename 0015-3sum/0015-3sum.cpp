b15c8edc09b2299891c6aa449fbae24354f58dd2
Suraj Vitthal Patil <145219543+SurajPatil2645@users.noreply.github.com>
2026-03-13 19:55:09 +0530

    Time: 45 ms (73.1%), Space: 29.1 MB (71.93%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        if (nums.size() < 3) return {};  // Handle small input edge case
+        int n=nums.size();
+        vector<vector<int>> ans;
 
         sort(nums.begin(), nums.end());
-        vector<vector<int>> answer;
-        answer.reserve(nums.size() / 3);  // Preallocate memory
-
-        for (int i = 0; i < nums.size() - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicate `i`
-
-            int j = i + 1, k = nums.size() - 1;
-            while (j < k) {
-                int localSum = nums[i] + nums[j] + nums[k];
-
-                if (localSum < 0) { j++; continue; }
-                if (localSum > 0) { k--; continue; }
-
-                answer.push_back({nums[i], nums[j], nums[k]});
-
-                // Skip duplicate `j` and `k`
-                do { j++; } while (j < k && nums[j] == nums[j - 1]);
-                do { k--; } while (j < k && nums[k] == nums[k + 1]);
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0) j++;
+                else if(sum>0) k--;
+                else{
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                }
             }
         }
-        return answer;
+        return ans;
     }
 };
