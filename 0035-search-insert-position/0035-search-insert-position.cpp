7f0f30dc808236b97a7cf3c781c2976f7d3111cc
Suraj Vitthal Patil <145219543+SurajPatil2645@users.noreply.github.com>
2026-04-01 23:37:32 +0530

    Time: 0 ms (100%), Space: 13.7 MB (42.29%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& arr, int target) {
+        int st = 0, end = arr.size()-1;
+        while(st < end){
+            int mid = st + (end - st)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(arr[mid] < target){
+                st = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        if(arr[st] < target){
+            return ++st;
+        }
+        return st;
+    }
+};
