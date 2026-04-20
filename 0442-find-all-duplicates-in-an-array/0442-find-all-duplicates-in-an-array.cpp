a8b52911d3f8a03a106e56496d48cbe2a3be8f8d
Suraj Vitthal Patil <145219543+SurajPatil2645@users.noreply.github.com>
2026-04-20 22:52:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,13 +1,32 @@
+// class Solution {
+// public:
+//     vector<int> findDuplicates(vector<int>& nums) {
+//         vector<int> ans;
+//         set<int> s;
+//         for(int val : nums){
+//             if(s.count(val)){
+//                 ans.push_back(val);
+//             } else{
+//                 s.insert(val);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> ans;
-        set<int> s;
-        for(int val : nums){
-            if(s.count(val)){
-                ans.push_back(val);
-            } else{
-                s.insert(val);
+        for (int i = 0; i < nums.size(); i++) {
+            int index = abs(nums[i]) - 1; // Use value as index
+            
+            // If the value at this index is negative, we've seen it before
+            if (nums[index] < 0) {
+                ans.push_back(abs(nums[i]));
+            } else {
+                // Mark as visited by making it negative
+                nums[index] = -nums[index];
             }
         }
         return ans;
