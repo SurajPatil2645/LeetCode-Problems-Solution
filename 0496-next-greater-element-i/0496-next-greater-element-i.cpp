48c66e0d21aea3859b06ff746b25633571126236
Suraj Vitthal Patil <145219543+SurajPatil2645@users.noreply.github.com>
2026-07-18 22:27:36 +0530

    Time: 2 ms (46.95%), Space: 12.8 MB (39.94%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,52 +1,26 @@
-// class Solution {
-// public:
-//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-//         vector<int> ans;
-//         for(int i = 0;i<nums1.size();i++){
-//             int cur = nums1[i];
-//             int idx = 0;
-//             bool found = false;
-//             while(nums2[idx] != cur){
-//                 idx++;
-//             }
-//             for(int j = idx;j<nums2.size();j++){
-//                 if(cur<nums2[j]){
-//                     ans.push_back(nums2[j]);
-//                     found = true;
-//                     break;
-//                 }
-//             }
-//             if(!found){
-//                 ans.push_back(-1);
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> next_greater;
+        unordered_map<int, int> m;
         stack<int> s;
 
-        for (int num : nums2) {
-            while (!s.empty() && s.top() < num) {
-                next_greater[s.top()] = num;
+        for(int i=nums2.size()-1; i>=0;i--){
+            while(s.size() > 0 && s.top() <= nums2[i]){
                 s.pop();
             }
-            s.push(num);
+            if(s.empty()){
+                m[nums2[i]] = -1;
+            } else{
+                m[nums2[i]] = s.top();
+            }
+            s.push(nums2[i]);
         }
 
         vector<int> ans;
-        for (int num : nums1) {
-            if (next_greater.count(num)) {
-                ans.push_back(next_greater[num]);
-            } else {
-                ans.push_back(-1);
-            }
+        for(int i=0;i<nums1.size();i++){
+            ans.push_back(m[nums1[i]]);
         }
-        
+
         return ans;
     }
 };
